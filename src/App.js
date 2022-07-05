b23ccad624251c174a30fcae413dfe9bfb8bfe51
jorgevictoria1986 <jorge_a2784@hotmail.com>
2022-07-04 23:55:40 -0300

    Desafio 2

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <nav className="navbar navbar-expand-lg bg-light">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">Calzados Silvia</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Damas</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Caballeros</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Niños</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
       </header>
     </div>
   );
