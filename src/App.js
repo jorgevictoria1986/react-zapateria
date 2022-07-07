8cd7f464d449c2c171190147972ec4ff15ad7a0f
jorgevictoria1986 <jorge_a2784@hotmail.com>
2022-07-06 22:38:58 -0300

    Desafio 3 v3

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/NavBar';
-import CartWidget from './components/CartWidget';
+
 import ItemListContainer from './components/ItemListContainer';
 
 function App() {
--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -3,9 +3,9 @@ import {ShoppingCartOutlined} from "@material-ui/icons";
 
 const CartWidget = () => {
 return (
-    <Budge badgeContent={4} >
+    <Badge badgeContent={4} >
         <ShoppingCartOutlined />
-    </Budge>
+    </Badge>
 );
 
 }
