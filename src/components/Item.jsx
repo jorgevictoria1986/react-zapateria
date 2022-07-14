d492bc60fb6fbba9b38383a31a840a53c507819f
jorgevictoria1986 <jorge_a2784@hotmail.com>
2022-07-14 13:31:42 -0300

    D6 V5

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,27 +1,27 @@
 import React from 'react'
 
-// const Item = (product) => {
-//   return (
-//     <div>
-//         <div>{product.tipo}</div>
-//         <div>{product.talles}</div>
-//         <div>{product.image}</div>
-//         <div>{product.precio}</div>
-
-//     </div>
-//   )
-// }
-
-const Item = (el) => {
+const Item = (product) => {
   return (
     <div>
-        <div>{el.tipo}</div>
-        <div>{el.talles}</div>
-        <div>{el.image}</div>
-        <div>{el.precio}</div>
+        <div>{product.tipo}</div>
+        <div>{product.talles}</div>
+        <div>{product.image}</div>
+        <div>{product.precio}</div>
 
     </div>
   )
 }
+
+// const Item = (el) => {
+//   return (
+//     <div>
+//         <div>{el.tipo}</div>
+//         <div>{el.talles}</div>
+//         <div>{el.image}</div>
+//         <div>{el.precio}</div>
+
+//     </div>
+//   )
+// }
 
 export default Item
--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,24 +1,24 @@
 import React from 'react'
 import Item from './Item'
 
-// const ItemList = ({items}) => {
-//   return (
-//     {
-//         items.map (item => <Item key={item.id} product={ item } /> )
-//     }
-//   )
-// }
-
-
-const ItemList = ({ products }) => {
+const ItemList = ({items}) => {
   return (
-    <div className="justify-content-center row row-cols-1 row-cols-sm-2 g-3">
-      {products.map((el) => (
-        <Item {...el} key={el.id} />
-      ))}
+    <div>
+    {items.map (item => <Item key={item.id} product={ item } /> )}
     </div>
-  );
-};
+  )
+}
+
+
+// const ItemList = ({ products }) => {
+//   return (
+//     <div className="justify-content-center row row-cols-1 row-cols-sm-2 g-3">
+//       {products.map((el) => (
+//         <Item {...el} key={el.id} />
+//       ))}
+//     </div>
+//   );
+// };
 
 
 
