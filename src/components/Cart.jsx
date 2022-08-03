3c111708055bd4e1b12a1958269bd65ad354ab71
jorgevictoria1986 <jorge_a2784@hotmail.com>
2022-08-02 23:51:50 -0300

    Desafio 10

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,56 +1,52 @@
 import { Link } from 'react-router-dom';
 import { useContext } from 'react';
 import { CartContext } from './CartContext';
-import { WrapperCart, TitleCart, ContentCart, Product, ProductDetail, ImageCart, Details, PriceDetail, ProductAmountContainer, ProductAmount, ProductPrice } from './styledComponents';
-import { Top, TopButton, TopText, Bottom, Button, Info, Summary, SummaryItem, SummaryItemPrice, SummaryItemText, SummaryTitle } from './styledComponents';
-
+import { WrapperCart, TitleCart, ContentCart, Product, ProductDetail, ImageCart, Details, PriceDetail, ProductAmountContainer, ProductAmount, ProductPrice, Top, TopButton, TopText, Summary, SummaryItem, SummaryTitle, SummaryItemText, SummaryItemPrice, Button } from './styledComponents';
 import FormatNumber from "./FormatNumber";
 // import styled from "styled-components";
 
 
 const Cart = () => {
     const test = useContext(CartContext);
+    // console.log(test.CartContext)
 
     return (
         <WrapperCart>
-            <TitleCart>Tu Carrito</TitleCart>
+            <TitleCart>Tu carrito</TitleCart>
             <Top>
-                <Link to='/'><TopButton>Continuar Comprando</TopButton></Link>
+                <Link to='/'><TopButton>Continuar Compranado</TopButton></Link>
                 {
                     (test.cartList.length > 0)
                     ? <TopButton type="filled" onClick={test.removeList}>Borrar Todo</TopButton>
-                    : <TopText>Carrito Vacío</TopText>
+                    : <TopText>Carrito Vacio</TopText>
                 }
             </Top>
             <ContentCart>
-            <Bottom>
-                <Info>
                     {
-                        test.cartList.length > 0 &&
-                            test.cartList.map(product => 
-                            <Product key={product.idItem}>
-                            <ProductDetail>
-                                <ImageCart src={product.imgItem} />
-                                <Details>
-                                <span>
-                                    <b>Producto:</b> {product.nameItem}
-                                </span>
-                                <TopButton type="filled" onClick={() => test.deleteItem(product.idItem)}>Borrar</TopButton>
-                                </Details>
-                            </ProductDetail>
-                            <PriceDetail>
-                                <ProductAmountContainer>
-                                <ProductAmount>{product.qtyItem} item(s)</ProductAmount>
-                                /
-                                <ProductAmount>$ {product.costItem} each</ProductAmount>
-                                </ProductAmountContainer>
-                                <ProductPrice>$ {test.calcTotalPerItem(product.idItem)} </ProductPrice>
-                            </PriceDetail>
-                            </Product>
-                            )
+                        test.cartList.length > 0 ? 
+                        test.cartList.map(product => 
+                        <Product key={product.idItem}>
+                        <ProductDetail>
+                            <ImageCart src={product.imgItem} />
+                            <Details>
+                            <span>
+                                <b>Product:</b> {product.nameItem}
+                            </span>
+                            <TopButton type="filled" onClick={() => test.deleteItem(product.idItem)}>Borrar</TopButton>
+                            </Details>
+                        </ProductDetail>
+                        <PriceDetail>
+                            <ProductAmountContainer>
+                            <ProductAmount>{product.qtyItem} item(s)</ProductAmount>
+                            </ProductAmountContainer>
+                            <ProductPrice> {product.costItem} each</ProductPrice>
+                        </PriceDetail>
+                        </Product>
+                        )
+                        : <TitleCart></TitleCart>
                     }
-                </Info>
-                {
+
+{
                     test.cartList.length > 0 &&
                         <Summary>
                             <SummaryTitle>Tu Orden</SummaryTitle>
@@ -73,10 +69,15 @@ const Cart = () => {
                             <Button>PAGAR</Button>
                         </Summary>
                 }
-            </Bottom>
+            
+
+
+
+
             </ContentCart>
         </WrapperCart>
     );
+    
 }
 
 export default Cart;
